b79d52e0da9c7946f98469d7185b43c7c620a338
Emad Ali <70924991+Mclilzee@users.noreply.github.com>
2026-09-11 23:42:07 +0200

    Fix build file

--- build.zig
+++ build.zig
@@ -108,12 +108,7 @@ pub fn build(b: *std.Build) void {
     // By making the run step depend on the default step, it will be run from the
     // installation directory rather than directly from within the cache directory.
     run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    run_cmd.addPassthruArgs();
 
     // Creates an executable that will run `test` blocks from the provided module.
     // Here `mod` needs to define a target, which is why earlier we made sure to
